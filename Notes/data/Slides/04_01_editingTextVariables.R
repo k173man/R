9a37e00a6d2f2509d75bf9a13f120abd5fbdbe01
k173man <shane.t.reed@gmail.com>
2015-03-14 21:42:37 -0400

    Updated slides code

--- Notes/data/Slides/04_01_editingTextVariables.R
+++ Notes/data/Slides/04_01_editingTextVariables.R
@@ -1,41 +1,45 @@
-if(!file.exists("./data")){dir.create("./data")}
+if(!file.exists("./data")) {
+	dir.create("./data")
+}
 fileUrl <- "https://data.baltimorecity.gov/api/views/dz54-2aru/rows.csv?accessType=DOWNLOAD"
 download.file(fileUrl,destfile="./data/cameras.csv",method="curl")
 cameraData <- read.csv("./data/cameras.csv")
 names(cameraData)
- tolower(names(cameraData))
- splitNames = strsplit(names(cameraData),"\\.")
+tolower(names(cameraData))
+splitNames = strsplit(names(cameraData),"\\.")
 splitNames[[5]]
- splitNames[[6]]
- mylist <- list(letters = c("A", "b", "c"), numbers = 1:3, matrix(1:25, ncol = 5))
+splitNames[[6]]
+mylist <- list(letters = c("A", "b", "c"), numbers = 1:3, matrix(1:25, ncol = 5))
 head(mylist)
- mylist[1]
- mylist$letters
- mylist[[1]]
- splitNames[[6]][1]
- firstElement <- function(x){x[1]}
+mylist[1]
+mylist$letters
+mylist[[1]]
+splitNames[[6]][1]
+firstElement <- function(x){x[1]}
 sapply(splitNames,firstElement)
- fileUrl1 <- "https://dl.dropboxusercontent.com/u/7710864/data/reviews-apr29.csv"
+fileUrl1 <- "https://dl.dropboxusercontent.com/u/7710864/data/reviews-apr29.csv"
 fileUrl2 <- "https://dl.dropboxusercontent.com/u/7710864/data/solutions-apr29.csv"
 download.file(fileUrl1,destfile="./data/reviews.csv",method="curl")
 download.file(fileUrl2,destfile="./data/solutions.csv",method="curl")
-reviews <- read.csv("./data/reviews.csv"); solutions <- read.csv("./data/solutions.csv")
+reviews <- read.csv("./data/reviews.csv")
+solutions <- read.csv("./data/solutions.csv")
 head(reviews,2)
- head(solutions,2)
- names(reviews)
- sub("_","",names(reviews),)
- testName <- "this_is_a_test"
+head(solutions,2)
+names(reviews)
+sub("_","",names(reviews),)
+testName <- "this_is_a_test"
 sub("_","",testName)
- gsub("_","",testName)
- grep("Alameda",cameraData$intersection)
- table(grepl("Alameda",cameraData$intersection))
- cameraData2 <- cameraData[!grepl("Alameda",cameraData$intersection),]
- grep("Alameda",cameraData$intersection,value=TRUE)
- grep("JeffStreet",cameraData$intersection)
- length(grep("JeffStreet",cameraData$intersection))
- library(stringr)
+gsub("_","",testName)
+grep("Alameda",cameraData$intersection)
+table(grepl("Alameda",cameraData$intersection))
+cameraData2 <- cameraData[!grepl("Alameda",cameraData$intersection),]
+grep("Alameda",cameraData$intersection,value=TRUE)
+grep("JeffStreet",cameraData$intersection)
+length(grep("JeffStreet",cameraData$intersection))
+
+library(stringr)
 nchar("Jeffrey Leek")
- substr("Jeffrey Leek",1,7)
- paste("Jeffrey","Leek")
- paste0("Jeffrey","Leek")
- str_trim("Jeff      ")
+substr("Jeffrey Leek",1,7)
+paste("Jeffrey","Leek")
+paste0("Jeffrey","Leek")
+str_trim("Jeff      ")
