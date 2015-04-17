c341cf160e0032d5a5f31b92ea221f6cf5a81db1
k173man <shane.t.reed@gmail.com>
2015-04-17 16:07:33 -0400

    partially complete

--- Coursera Data Science/04 Exploratory Data Analysis/Project 2/plot5.R
+++ Coursera Data Science/04 Exploratory Data Analysis/Project 2/plot5.R
@@ -1,4 +1,14 @@
+library(dplyr)
+
 setwd("/Docs/tmp/data")
 
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+
+BcMv <- NEI %>% 
+  filter(fips == "24510" & type == "ON-ROAD") %>% 
+  select(year, Emissions) %>% 
+  group_by(year) %>% 
+  summarize(TotalEmissions = sum(Emissions))
+
+with(BcMv, plot(year, TotalEmissions, type = "l"))
