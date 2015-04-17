806b1911f7c6bda8491356647728b70038156cd5
k173man <shane.t.reed@gmail.com>
2015-04-17 16:06:50 -0400

    partially complete

--- Coursera Data Science/04 Exploratory Data Analysis/Project 2/plot4.R
+++ Coursera Data Science/04 Exploratory Data Analysis/Project 2/plot4.R
@@ -1,4 +1,22 @@
+library(dplyr)
+library(data.table)
+
 setwd("/Docs/tmp/data")
 
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+
+cc <- SCC %>% 
+  filter(grepl("coal", Short.Name, ignore.case = T) & grepl("combustion", SCC.Level.One, ignore.case = T)) %>% 
+  select(SCC)
+
+# factor(...) drops unused factors
+dtScc <- data.table(SCC = factor(cc$SCC))
+dtNEI <- data.table(SCC = NEI$SCC, Emissions = NEI$Emissions, year = NEI$year)
+
+dtCC <- merge(dtNEI, dtScc, by = "SCC") %>% 
+  select(year, Emissions) %>% 
+  group_by(year) %>% 
+  summarize(TotalEmissions = sum(Emissions))
+
+with(dtCC, plot(year, TotalEmissions, type = "l"))
