2543e301630229bfbcc9fb79a59d24527eeeb440
k173man <shane.t.reed@gmail.com>
2015-04-20 21:48:40 -0400

    update - EDA - project 2

--- Coursera Data Science/04 Exploratory Data Analysis/Project 2/plot2.R
+++ Coursera Data Science/04 Exploratory Data Analysis/Project 2/plot2.R
@@ -2,13 +2,25 @@ library(dplyr)
 
 setwd("/Docs/tmp/data")
 
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
+# if(file.exists("Source_Classification_Code.rds")){
+#     SCC <- readRDS("Source_Classification_Code.rds")
+# } else {
+#     stop("Source_Classification_Code.rds file is missing")
+# }
 
-d <- NEI %>% 
-    filter(fips = "24510") %>% 
+if(file.exists("summarySCC_PM25.rds")){
+    NEI <- readRDS("summarySCC_PM25.rds")
+} else {
+    stop("summarySCC_PM25.rds file is missing")
+}
+
+# Subset data for Baltimore City, Maryland, and then summarize Emissions by year
+tebc <- NEI %>% 
+    filter(fips == "24510") %>% 
     select(year, Emissions) %>% 
     group_by(year) %>% 
     summarize(TotalEmissions = sum(Emissions))
 
-with(d, plot(year, TotalEmissions, type = "l"))
+# plot data to determine if total emissions from PM2.5 have decreased in the Baltimore City, Maryland from 1999 to 2008
+with(tebc, plot(year, TotalEmissions, type = "l"))
+
