6753c4fd854cac50ab0267064c9db9b019a848d9
k173man <shane.t.reed@gmail.com>
2015-10-20 19:07:02 -0400

    Update Project 2.R

--- Coursera Data Science/05 Reproducible Research/Project 2/Project 2.R
+++ Coursera Data Science/05 Reproducible Research/Project 2/Project 2.R
@@ -1,1 +1,9 @@
-# Place Holder
+library(dplyr)
+library(ggplot2)
+library(lattice)
+
+setwd("~/DataSci/R/Coursera Data Science/05 Reproducible Research/Project 2")
+
+# stormData <- read.csv("data/repdata_data_StormData.csv.bz2")
+# save(stormData, file="data/stormData.RData", compress="bzip2")
+load("data/stormData.RData")
