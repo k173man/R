1cc1395f22a26123e9881ae738ac9a2a85acdd6f
k173man <shane.t.reed@gmail.com>
2015-11-13 16:46:08 -0500

    Create Quiz #2.R

---
+++ Coursera Data Science/06 Statistical Inference/Quizes/Quiz #2.R
@@ -0,0 +1,19 @@
+# Q2 - units are expressed as mm Hg
+## Diastolic blood pressures (DBPs) for men aged 35-44 are normally distributed (u = 80, sd = 10)
+## What is the approx. probability that a random 35-44 year old has a DBP < 70?
+u = 80
+sd = 10
+x = 70
+
+pnorm(x, u, sd)
+
+# Q3 - units are expressed as cc
+## Brain volume for adult women is normally distributed (u = 1,100, sd = 75)
+## What brain volume represents the 95th percentile?
+u = 1100
+sd = 75
+x = 70
+
+pnorm(x, u, sd)
+
+
