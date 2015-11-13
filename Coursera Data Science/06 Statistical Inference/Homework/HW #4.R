1c713e5fc22a803d87db159f3cbd538063ed0ef3
k173man <shane.t.reed@gmail.com>
2015-11-13 16:52:57 -0500

    Added empty HW & Quiz files

---
+++ Coursera Data Science/06 Statistical Inference/Homework/HW #4.R
@@ -0,0 +1,29 @@
+# Q1
+
+
+# Q2 - units expressed as hits/day
+
+
+# Q3 - Notation: A = Asbestos, Ac = Compliment of A, Tn = Negative Test
+
+
+# Q4 - units expressed as hits/day
+
+
+# Q5 - units expressed as hits/day
+
+
+# Q6 - Taste test to determine if testee can distinguish b/t cheap & expensive wines, i.e. binomial, where p =.5
+
+
+# Q7 - uniform distribution will use xnorm() functions
+
+
+# Q8
+
+
+# Q9
+
+
+# Q10
+
