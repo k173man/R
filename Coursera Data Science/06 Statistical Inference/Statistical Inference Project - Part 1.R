116c68c91e9c18d243de511643274b0a54efa15c
k173man <shane.t.reed@gmail.com>
2015-11-17 12:46:16 -0500

    Update and rename Statistical Inference Project.R to Statistical Inference Project - Part 1.R

--- Coursera Data Science/06 Statistical Inference/Statistical Inference Project - Part 1.R
+++ Coursera Data Science/06 Statistical Inference/Statistical Inference Project - Part 1.R
@@ -1,35 +1,23 @@
 library(ggplot2)
 
-# rexp(n, lambda) where lambda is the rate parameter.
-#     The mean of exponential distribution is 1/lambda
-#     The standard deviation is also 1/lambda
-# Set lambda = 0.2 for all of the simulations.
-# You will investigate the distribution of averages of 40 exponentials.
-# Note that you will need to do a thousand simulations.
-
-# Illustrate, via simulation and associated explanatory text, the properties of the distribution of the mean of 40 exponentials.
-# 1. Show the sample mean and compare it to the theoretical mean of the distribution.
-# 2. Show how variable the sample is (via variance) and compare it to the theoretical variance of the distribution.
-# 3. Show that the distribution is approximately normal.
-#     - the difference between the distribution of a large collection of random exponentials
-#     - the distribution of a large collection of averages of 40 exponentials. 
-
 noSim <- 1000
 sampleSize <- 40
 lambda <- .2
-stdError <- function(lmbda, n) 1/(lmbda*sqrt(n))
 
-theoreticalMean <- 1/lambda
-theoreticalVariance <- stdError(lambda, sampleSize)^2
-# using noSim * sampleSize so, for Q3, we are comparing the set of numbers
 rv <- rexp(noSim * sampleSize, lambda)
+
 randomExps <- data.frame(
-  largeSample = sample(rv, noSim),
-  sampleMeans = rowMeans(matrix(rv, noSim))
+  sampleMeans = rowMeans(matrix(rv, noSim)), 
+  largeSample = sample(rv, noSim) 
 )
 
 sampleMean <- mean(randomExps$sampleMeans)
+theoreticalMean <- 1/lambda
+
+stdError <- function(lmbda, n) 1/(lmbda*sqrt(n))
+
 sampleVariance <- var(randomExps$sampleMeans)
+theoreticalVariance <- stdError(lambda, sampleSize)^2
 
 # Shows mean converging on theoretic mean as sample size increases
 means <- cumsum(randomExps$sampleMeans) / (1 : noSim)
@@ -44,7 +32,6 @@ g <- ggplot(data.frame(x = 1 : noSim, y = vars), aes(x = x, y = y))
 g <- g + geom_hline(yintercept = theoreticalVariance, color = "red") + geom_line(size = 1, color = "green")
 g <- g + labs(x = "Number of Observations", y = "Cumulative Variance")
 g
-
 
 g <- ggplot(randomExps, aes(x = largeSample)) + geom_histogram(color = "black", aes(y = ..density.., fill = ..density..)) + guides(fill=FALSE)
 g <- g + stat_function(fun = dexp, args = list(rate = lambda), size = 2)
